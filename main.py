c0e44bf662418c33f114e4843a85cfbada91ff62
Markel <markel@deusto-2022-02.>
2025-04-04 13:32:58 +0200

    Añadir la interfaz gráfica

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # main.py
 from flask import Flask
 from app.api.routes import api
+from app.ui.interfaz import ui  # Importar la interfaz web
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(api)
+    app.register_blueprint(ui)  # Registrar la interfaz web
     return app
 
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
-
